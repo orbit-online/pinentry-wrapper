a3088de6f1b6a4bd9927220936b1eb6960daf84b
Anders Ingemann <aim@orbit.online>
2023-06-30 11:54:41 +0200

    Allow params for pinentry to be set via environment variables

--- pinentry-wrapper.sh
+++ pinentry-wrapper.sh
@@ -11,35 +11,44 @@ pinentry_wrapper() {
 
   DOC="Pinentry Wrapper - Cross-platform pinentry script with a CLI interface
 Usage:
-  pinentry-wrapper [options] PROMPT
+  pinentry-wrapper [options] [PROMPT]
 
 Options:
   -d --desc TEXT    Text to appear below the prompt
-  -o --ok TEXT      Text for the OK button [default: OK]
-  -c --cancel TEXT  Text for the Cancel button [default: Cancel]
+                    [default: \${PINENTRY_DESC:-}]
+  -o --ok TEXT      Text for the OK button
+                    [default: \${PINENTRY_OK:-OK}]
+  -c --cancel TEXT  Text for the Cancel button
+                    [default: \${PINENTRY_CANCEL:-Cancel}]
   -e --error TEXT   Set an error text
+                    [default: \${PINENTRY_ERROR:-}]
+
+Note:
+  PROMPT can be overriden with \$PINENTRY_PROMPT
+  If PROMPT is not defined or overridden, the default is 'Enter your password'
 "
 # docopt parser below, refresh this parser with `docopt.sh pinentry-wrapper.sh`
 # shellcheck disable=2016,1090,1091,2034
 docopt() { source "$pkgroot/.upkg/andsens/docopt.sh/docopt-lib.sh" '1.0.0' || {
 ret=$?; printf -- "exit %d\n" "$ret"; exit "$ret"; }; set -e
-trimmed_doc=${DOC:0:335}; usage=${DOC:71:42}; digest=64ce0; shorts=(-c -d -o -e)
-longs=(--cancel --desc --ok --error); argcounts=(1 1 1 1); node_0(){
-value __cancel 0; }; node_1(){ value __desc 1; }; node_2(){ value __ok 2; }
-node_3(){ value __error 3; }; node_4(){ value PROMPT a; }; node_5(){
-optional 0 1 2 3; }; node_6(){ optional 5; }; node_7(){ required 6 4; }
-node_8(){ required 7; }; cat <<<' docopt_exit() {
-[[ -n $1 ]] && printf "%s\n" "$1" >&2; printf "%s\n" "${DOC:71:42}" >&2; exit 1
-}'; unset var___cancel var___desc var___ok var___error var_PROMPT; parse 8 "$@"
-local prefix=${DOCOPT_PREFIX:-''}; unset "${prefix}__cancel" "${prefix}__desc" \
-"${prefix}__ok" "${prefix}__error" "${prefix}PROMPT"
-eval "${prefix}"'__cancel=${var___cancel:-Cancel}'
-eval "${prefix}"'__desc=${var___desc:-}'; eval "${prefix}"'__ok=${var___ok:-OK}'
-eval "${prefix}"'__error=${var___error:-}'
+trimmed_doc=${DOC:0:648}; usage=${DOC:71:44}; digest=603ae; shorts=(-d -e -c -o)
+longs=(--desc --error --cancel --ok); argcounts=(1 1 1 1); node_0(){
+value __desc 0; }; node_1(){ value __error 1; }; node_2(){ value __cancel 2; }
+node_3(){ value __ok 3; }; node_4(){ value PROMPT a; }; node_5(){
+optional 0 1 2 3; }; node_6(){ optional 5; }; node_7(){ optional 4; }; node_8(){
+required 6 7; }; node_9(){ required 8; }; cat <<<' docopt_exit() {
+[[ -n $1 ]] && printf "%s\n" "$1" >&2; printf "%s\n" "${DOC:71:44}" >&2; exit 1
+}'; unset var___desc var___error var___cancel var___ok var_PROMPT; parse 9 "$@"
+local prefix=${DOCOPT_PREFIX:-''}; unset "${prefix}__desc" "${prefix}__error" \
+"${prefix}__cancel" "${prefix}__ok" "${prefix}PROMPT"
+eval "${prefix}"'__desc=${var___desc:-'"'"'${PINENTRY_DESC:-}'"'"'}'
+eval "${prefix}"'__error=${var___error:-'"'"'${PINENTRY_ERROR:-}'"'"'}'
+eval "${prefix}"'__cancel=${var___cancel:-'"'"'${PINENTRY_CANCEL:-Cancel}'"'"'}'
+eval "${prefix}"'__ok=${var___ok:-'"'"'${PINENTRY_OK:-OK}'"'"'}'
 eval "${prefix}"'PROMPT=${var_PROMPT:-}'; local docopt_i=1
 [[ $BASH_VERSION =~ ^4.3 ]] && docopt_i=2; for ((;docopt_i>0;docopt_i--)); do
-declare -p "${prefix}__cancel" "${prefix}__desc" "${prefix}__ok" \
-"${prefix}__error" "${prefix}PROMPT"; done; }
+declare -p "${prefix}__desc" "${prefix}__error" "${prefix}__cancel" \
+"${prefix}__ok" "${prefix}PROMPT"; done; }
 # docopt parser above, complete command for generating this parser is `docopt.sh --library='"$pkgroot/.upkg/andsens/docopt.sh/docopt-lib.sh"' pinentry-wrapper.sh`
   eval "$(docopt "$@")"
 
@@ -57,11 +66,31 @@ declare -p "${prefix}__cancel" "${prefix}__desc" "${prefix}__ok" \
   verbose "pinentry command found: '%s'" "$pinentry_cmd"
 
   local commands=() pinentry_script out
-  commands+=("SETPROMPT $PROMPT")
-  [[ -z $__desc ]] || commands+=("SETDESC $__desc")
-  [[ -z $__ok ]] || commands+=("SETOK $__ok")
-  [[ -z $__cancel ]] || commands+=("SETCANCEL $__cancel")
-  [[ -z $__error ]] || commands+=("SETERROR $__error")
+  if [[ -n $PINENTRY_PROMPT ]]; then
+    commands+=("SETPROMPT $PINENTRY_PROMPT")
+  else
+    commands+=("SETPROMPT ${PROMPT:-Enter your password}")
+  fi
+  if [[ $__desc = "\${PINENTRY_DESC:-}" ]]; then
+    [[ -z $PINENTRY_DESC ]] || commands+=("SETDESC $PINENTRY_DESC")
+  elif [[ -n $__desc ]]; then
+    commands+=("SETDESC $__desc")
+  fi
+  if [[ $__ok = "\${PINENTRY_OK:-OK}" ]]; then
+    commands+=("SETOK ${PINENTRY_OK:-OK}")
+  elif [[ -n $__ok ]]; then
+    commands+=("SETOK $__ok")
+  fi
+  if [[ $__cancel = "\${PINENTRY_CANCEL:-Cancel}" ]]; then
+    commands+=("SETCANCEL ${PINENTRY_CANCEL:-Cancel}")
+  elif [[ -n $__cancel ]]; then
+    commands+=("SETCANCEL $__cancel")
+  fi
+  if [[ $__error = "\${PINENTRY_ERROR:-}" ]]; then
+    [[ -z $PINENTRY_ERROR ]] || commands+=("SETERROR $PINENTRY_ERROR")
+  elif [[ -n $__error ]]; then
+    commands+=("SETERROR $__error")
+  fi
   commands+=("GETPIN")
   join_by() { local IFS="$1"; shift; echo "$*"; }
   pinentry_script=$(join_by $'\n' "${commands[@]}")
